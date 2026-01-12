8e81ef29f15f9a9b011ba768b72ee769699cb728
Jakub <jakub2002t@gmail.com>
2026-01-12 16:33:58 +0100

    Configure ESLint for test files

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,8 @@ export default [
     },
     languageOptions: {
       parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
         ecmaFeatures: {
           jsx: true,
         },
@@ -19,11 +21,47 @@ export default [
         document: "readonly",
         console: "readonly",
         process: "readonly",
+        React: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
     rules: {
       "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^(React|StrictMode)$",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/main.jsx", "**/index.jsx"],
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    files: ["**/*.test.{js,jsx}", "**/*.spec.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        test: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        vi: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
     },
   },
 ];
